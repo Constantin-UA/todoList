8fac3ff0d516c08b5ced76cedf0eaa3499a9770a
Constantin-UA <konstantinsh579@gmail.com>
2022-02-22 18:42:59 +0200

    complet 4 step

--- app.js
+++ app.js
@@ -1,11 +1,13 @@
 // Globals
 const todoList = document.getElementById('todo-list');
+const userSelect = document.getElementById('user-todo');
+const form = document.querySelector('form');
 let todos = [];
 let users = [];
 
 // Attach Events
 document.addEventListener('DOMContentLoaded', initApp);
-
+form.addEventListener('submit', handleSubmit);
 // Basic logic
 function getUserName(userId) {
 	const user = users.find((u) => u.id === userId);
@@ -30,7 +32,13 @@ function printTodo({ id, userId, title, completed }) {
 
 	todoList.prepend(li);
 }
+function createUserOption(user) {
+	const option = document.createElement('option');
+	option.value = user.id;
+	option.innerText = user.name;
 
+	userSelect.append(option);
+}
 // Event logic
 function initApp() {
 	Promise.all([getAllTodos(), getAllUsers()]).then((values) => {
@@ -38,6 +46,16 @@ function initApp() {
 
 		//Отправить в разметку
 		todos.forEach((todo) => printTodo(todo));
+		users.forEach((user) => createUserOption(user));
+	});
+}
+function handleSubmit(event) {
+	event.preventDefault();
+
+	createTodo({
+		userId: Number(form.user.value),
+		title: form.todo.value,
+		completed: false,
 	});
 }
 
@@ -54,4 +72,18 @@ async function getAllUsers() {
 	const data = await response.json();
 
 	return data;
+}
+
+async function createTodo(todo) {
+	const response = await fetch('https://jsonplaceholder.typicode.com/todos', {
+		method: 'POST',
+		body: JSON.stringify(todo),
+		headers: {
+			'Content-Type': 'application/json',
+		},
+	});
+
+	const newTodo = await response.json();
+
+	printTodo(newTodo);
 }
